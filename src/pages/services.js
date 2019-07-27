5b48ab83a5f8c2874394f293b0809e98635a1a34
Josh Kneeland <joshkneeland@Joshs-MacBook-Pro-2.local>
2019-07-27 16:15:36 -0600

    Starting to implement code for gatsbyjs crash course

---
+++ src/pages/services.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServicesPage = () =>
+    <div>
+        <h1>Services</h1>
+        <p>Incididunt reprehenderit sunt aliquip aliquip sit nulla do sunt amet do adipisicing in duis. Exercitation culpa reprehenderit minim culpa ut esse nisi. Magna dolor dolor laborum fugiat aliqua laborum duis non. Non velit nulla laborum ullamco minim laborum esse consequat. Ut consequat consequat enim aliqua veniam id in.</p>
+    </div>
+
+export default ServicesPage;
