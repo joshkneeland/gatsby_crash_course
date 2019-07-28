b98b677e4d4a1028a10dc32ad172d791dce34cf3
Josh Kneeland <joshkneeland@Joshs-MacBook-Pro-2.local>
2019-07-27 18:57:57 -0600

    Played around w/graphQL configuration

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link  from 'gatsby/link';
+import { Link } from 'gatsby';
 
 const Menu = () =>
     <div style={{
@@ -14,6 +14,7 @@ const Menu = () =>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/services">Services</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
         </ul>
     </div>
 
---
+++ src/pages/post-two/index.md
@@ -0,0 +1,8 @@
+---
+path: "/post-two"
+date: "2018-04-010"
+title: "My Second Gatsby Post"
+author: "John Doe"
+---
+
+This is my second blog first in Gatsby
